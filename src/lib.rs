dcf46107266893dc0de872d01dc0f91c67008090
YoEight <yo.eight@gmail.com>
2023-07-29 22:44:07 -0400

    Expose history backend types.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,7 @@ mod input;
 mod persistence;
 
 pub use input::{file_backed_inputs, in_memory_inputs, params::Params, Input, Inputs, Options};
+pub use persistence::{FileBackend, Noop};
+
+pub type FileBackedInputs = Inputs<FileBackend>;
+pub type MemoryBackedInputs = Inputs<Noop>;
